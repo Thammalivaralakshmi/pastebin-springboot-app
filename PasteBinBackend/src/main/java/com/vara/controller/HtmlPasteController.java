c15dfa93fef66a92424faf489cee39b7f36a8e5d
Thammalivaralakshmi <129669607+Thammalivaralakshmi@users.noreply.github.com>
2026-01-28 21:52:09 +0530

    Update HtmlPasteController.java

--- PasteBinBackend/src/main/java/com/vara/controller/HtmlPasteController.java
+++ PasteBinBackend/src/main/java/com/vara/controller/HtmlPasteController.java
@@ -20,24 +20,44 @@ public class HtmlPasteController {
 		this.pasteService = pasteService;
 	}
 
+	// @GetMapping("/p/{id}")
+	// public ResponseEntity<?> getPaste(@PathVariable String id) {
+	//     Paste paste = pasteService.fetch(id, Instant.now());
+	//     if (paste == null) {
+	//         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+	//                              .body(Map.of("error", "Paste not found or expired"));
+	//     }
+	//     Integer remainingViews = 0;
+ //        if (paste.getMaxViews() != null) {
+ //            remainingViews = paste.getMaxViews() - paste.getViewCount();
+ //            if (remainingViews < 0) remainingViews = 0; // safety
+ //        }
+	//     return ResponseEntity.ok(Map.of(
+	//         "content", paste.getContent(),
+	//         "expires_at", paste.getExpiresAt(),
+	//         "view_count", paste.getViewCount(),
+	//         "max_views", paste.getMaxViews(),
+	//         "remaining_views", remainingViews
+	//     ));
+	// }
 	@GetMapping("/p/{id}")
-	public ResponseEntity<?> getPaste(@PathVariable String id) {
-	    Paste paste = pasteService.fetch(id, Instant.now());
-	    if (paste == null) {
-	        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-	                             .body(Map.of("error", "Paste not found or expired"));
-	    }
-	    Integer remainingViews = 0;
-        if (paste.getMaxViews() != null) {
-            remainingViews = paste.getMaxViews() - paste.getViewCount();
-            if (remainingViews < 0) remainingViews = 0; // safety
-        }
-	    return ResponseEntity.ok(Map.of(
-	        "content", paste.getContent(),
-	        "expires_at", paste.getExpiresAt(),
-	        "view_count", paste.getViewCount(),
-	        "max_views", paste.getMaxViews(),
-	        "remaining_views", remainingViews
-	    ));
+	@ResponseBody
+	public ResponseEntity<String> getPaste(@PathVariable String id) {
+   		Paste paste = pasteService.fetch(id, Instant.now());
+    	if (paste == null) {
+        	return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body("<h1>Paste not found or expired</h1>");
+    	}
+
+    	String safeContent = paste.getContent()
+                              .replaceAll("<", "&lt;")
+                              .replaceAll(">", "&gt;"); // prevent script injection
+
+    	String html = "<html><body>" +
+                  "<h2>Paste Content</h2>" +
+                  "<pre>" + safeContent + "</pre>" +
+                  "</body></html>";
+
+    	return ResponseEntity.ok(html);
 	}
 }
