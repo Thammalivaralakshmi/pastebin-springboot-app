5afea9d23ed225fb9d4a7691a8b3a908264d8213
Thammalivaralakshmi <129669607+Thammalivaralakshmi@users.noreply.github.com>
2026-01-28 21:20:44 +0530

    Update CorsConfig.java

--- PasteBinBackend/src/main/java/com/vara/config/CorsConfig.java
+++ PasteBinBackend/src/main/java/com/vara/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://pastebin-springboot-app.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedMethods("*");
             }
         };
