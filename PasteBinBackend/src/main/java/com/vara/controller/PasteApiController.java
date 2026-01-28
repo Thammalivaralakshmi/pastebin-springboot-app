6652c284082fefbf4a961105136e766d77281a2e
Thammalivaralakshmi <129669607+Thammalivaralakshmi@users.noreply.github.com>
2026-01-28 23:14:19 +0530

    Update PasteApiController.java

--- PasteBinBackend/src/main/java/com/vara/controller/PasteApiController.java
+++ PasteBinBackend/src/main/java/com/vara/controller/PasteApiController.java
@@ -72,26 +72,28 @@ public class PasteApiController {
         );
     }
 
-    @GetMapping("/api/pastes/{id}")
-    public ResponseEntity<?> getPaste(@PathVariable String id) {
-        Paste paste = pasteService.fetch(id, Instant.now());
-        if (paste == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                 .body(Map.of("error", "Paste not found or expired"));
-        }
+   @GetMapping("/api/pastes/{id}")
+public ResponseEntity<?> getPaste(@PathVariable String id, HttpServletRequest request) {
+    Instant now = timeProvider.now(request);
+    Paste paste = pasteService.fetch(id, now);
 
-        Integer remainingViews = 0;
-        if (paste.getMaxViews() != null) {
-            remainingViews = paste.getMaxViews() - paste.getViewCount();
-            if (remainingViews < 0) remainingViews = 0; // safety
-        }
-
-        return ResponseEntity.ok(Map.of(
-        	    "content", paste.getContent(),
-        	    "expires_at", paste.getExpiresAt(),
-        	    "view_count", paste.getViewCount(),
-        	    "max_views", paste.getMaxViews(),
-        	    "remaining_views", remainingViews
-        	));
+    if (paste == null) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(Map.of("error", "Paste not found or expired"));
     }
+
+    Integer remainingViews = 0;
+    if (paste.getMaxViews() != null) {
+        remainingViews = paste.getMaxViews() - paste.getViewCount();
+        if (remainingViews < 0) remainingViews = 0; // safety
+    }
+
+    return ResponseEntity.ok(Map.of(
+        "content", paste.getContent(),
+        "expires_at", paste.getExpiresAt(),
+        "view_count", paste.getViewCount(),
+        "max_views", paste.getMaxViews(),
+        "remaining_views", remainingViews
+    ));
+}
 }
